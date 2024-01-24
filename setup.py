7e90e74e7ff2a018b80d47e43e980e18b474b0ed
Pallavi-Sinha-12 <pallavisinha95829@gmail.com>
2024-01-24 11:08:51 +0530

    Update setup files

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open("requirements.txt") as f:
 
 setup(
     name="bitefix",
-    version="0.0.2",
-    description="""BiteFix is a powerful and efficient package designed to automate and streamline the process of error fixing. 
+    version="0.0.3",
+    description="""Bitefix is an efficient library designed to streamline error fixing with AI-powered decorators for seamless debugging. 
     It initializes a crew of AI agents which work together to diagnose the error, generate ideas to fix the error, 
     evaluate the ideas to choose the best and develop the code to fix the error.""",
     long_description=long_description,
